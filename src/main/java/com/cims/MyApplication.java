92d8ee142bbfad07f010863c4abf585e7c0214e0
仲宇泽 <291821441@qq.com>
2022-03-16 22:32:43 +0800

    测试分支提交

--- src/main/java/com/cims/MyApplication.java
+++ src/main/java/com/cims/MyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {
     //定义一个启动方法
     public static void main(String[] args) {
+        System.out.println("11111111");
         SpringApplication.run(MyApplication.class, args);
     }
 }
